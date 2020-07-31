3dabf9fee1708714107886e1dc9409d7fac2b5a4
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-07-31 01:24:27 -0400

    offline and online work correctly

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,8 @@
 //chat orig
-import React, { Component } from "react";
-import { View, Text, Platform } from "react-native";
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import React from "react";
+import { View, Text, AsyncStorage } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 const firebase = require("firebase");
 require("firebase/firestore");
 
@@ -10,6 +11,18 @@ export default class Chat extends React.Component {
 
   constructor() {
     super();
+
+    this.state = {
+      messages: [],
+      uid: "",
+      user: {
+        _id: "",
+        name: "",
+        avatar: "",
+      },
+      isConnected: true,
+    };
+
     if (!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: "AIzaSyCwiEp27P3CiEB_p4KT9-KxvUb5axjW_WE",
@@ -24,72 +37,50 @@ export default class Chat extends React.Component {
     //Read all documents in the messages collection
     this.referenceMessages = firebase.firestore().collection("usermessages");
 
-    //default state
-    this.state = {
-      messages: [],
-      uid: '',
-      user: {
-        _id: "",
-        name: "",
-      }
-    };
+
   }
-
-  onCollectionUpdate = (querySnapshot) => {
-    const messages = []; //go through each document
-    querySnapshot.forEach((doc) => {
-      //get the QueryDocumentShapshot's data
-      const data = doc.data();
-      messages.push({
-        _id: data._id, 
-        text: data.text,
-        createdAt: data.createdAt.toDate(),
-        user: data.user,
-      });
-    });
-    this.setState({
-      messages,
-    });
-    console.log(this.state.messages)
-  };
-
 
   componentDidMount() {
-
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
-      if (!user) {
-        await  firebase.auth().signInAnonymously();
+    NetInfo.isConnected.fetch().then(isConnected => {
+      this.setState({ isConnected: true });
+      if (isConnected === true) {
+        this.authUnsubscribe = firebase
+          .auth()
+          .onAuthStateChanged(async (user) => {
+            if (!user) {
+              await firebase.auth().signInAnonymously();
+            } else {
+              //update user state with currently active user data
+              this.setState({
+                uid: user.uid,
+                loggedInText: "Hello there",
+              });
+              console.log("online");
+            }
+            this.unsubscribe = this.referenceMessages
+              .orderBy("createdAt", "desc")
+              .onSnapshot(this.onCollectionUpdate);
+          });
       } else {
-        //update user state with currently active user data
-        this.setState({
-          uid: user.uid,
-          loggedInText: "Hello there",
-        });
+        console.log("offline");
+        this.setState({ isConnected: false });
+        this.getMsgs();
       }
-      this.unsubscribe = this.referenceMessages.orderBy('createdAt','desc').onSnapshot(
-        this.onCollectionUpdate
-      );
-
 
     });
+
+
+    NetInfo.isConnected.fetch().then(isConnected => {
+      console.log('First, is ' + (isConnected ? 'online' : 'offline'));
+    });
+
+
   }
+
 
   componentWillUnmount() {
     this.authUnsubscribe();
     this.unsubscribe();
- 
-  }
-
-
-
-  addMsg = () => {
-    const message = this.state.messages[0];
-    this.referenceMessages.add({
-      _id: message._id,
-      text: message.text || '',
-      createdAt: message.createdAt,
-      user: message.user
-    });
   }
 
   onSend(messages = []) {
@@ -99,9 +90,75 @@ export default class Chat extends React.Component {
       }),
       () => {
         this.addMsg();
+        this.saveMsgs();
       }
     );
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = []; //go through each document
+    querySnapshot.forEach((doc) => {
+      //get the QueryDocumentShapshot's data
+      const data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  addMsg = () => {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: message.user,
+      uid: this.state.uid,
+    });
+  };
+
+
+
+  async getMsgs() {
+    let messages = "";
+    try {
+      messages = (await AsyncStorage.getItem("messages")) || [];
+      this.setState({
+        messages: JSON.parse(messages),
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+
+  async saveMsgs() {
+    try {
+      await AsyncStorage.setItem(
+        "messages",
+        JSON.stringify(this.state.messages)
+      );
+    } catch (error) {
+       console.log(error.message);
+    }
+  }
+
+
+
+  // async deleteMsgs() {
+  //   try {
+  //     await AsyncStorage.removeItem("messages");
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // }
+
 
 
   renderBubble(props) {
@@ -110,14 +167,22 @@ export default class Chat extends React.Component {
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: "#000",
+            backgroundColor: "#4C6475",
           },
         }}
       />
     );
   }
+
+  renderInputToolbar(props) {
+    if (this.state.isConnected == false) {
+    } else {
+      return <InputToolbar {...props} />;
+    }
+  }
+
   render() {
-    console.log(this.state.uid)
+    //console.log(this.state.isConnected)
     return (
       <View
         style={[
@@ -130,13 +195,17 @@ export default class Chat extends React.Component {
       >
         <GiftedChat //Componenet comes with its own props. Providing GC with messages and info about the usesr
           renderBubble={this.renderBubble.bind(this)} //Chat bubble
+          renderInputToolbar={this.renderInputToolbar.bind(this)} //Hide if offline
           messages={this.state.messages} //State messages will be deisplayed
           onSend={(messages) => this.onSend(messages)} //Whe user sends messages append this ID
           user={{
-            _id: this.state.uid
+            _id: this.state.uid,
           }}
-        /> 
+        />
       </View>
     );
   }
+
+ 
+
 }
